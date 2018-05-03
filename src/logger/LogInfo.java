14763b4cf1be9feb5bf20b8b2a7063980837bd92
Kunal Ashar <kunalashar.25@gmail.com>
2018-05-03 19:09:26 +0530

    different logs file generated.

--- src/logger/LogInfo.java
+++ src/logger/LogInfo.java
@@ -1,19 +1,37 @@
 package logger;
 
-import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.log4j.Logger;
 
 public class LogInfo
 {
 
-	public static void main(String[] args)
-	{		
+	public static void main(String[] args) throws IOException
+	{
 		Logger logger = Logger.getLogger(LogInfo.class.getName());
 		
+		//to delete log files
+		deleteLogFiles();
+
 		logger.info("This is Info");
 		logger.error("This is Error");
 		logger.warn("This is Warning");
 		logger.debug("This is Debug");
-		logger.fatal("This is Fatal");		
+		logger.fatal("This is Fatal");
+	}
+
+	public static void deleteLogFiles()
+
+	{
+		File files = new File(System.getProperty("user.dir") + "\\Logs");
+
+		String[] entries = files.list();
+		for (String s : entries)
+		{
+			File currentFile = new File(files.getPath(), s);
+			currentFile.delete();
+		}
 	}
 }
